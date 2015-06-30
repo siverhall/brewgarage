814c4253afef127a0052ad1114eb65721880149e
siverhall <emil.siverhall@gmail.com>
2015-06-30 19:39:05 +0200

    transferred angular base to node/express app

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./api/routes/index');
+var users = require('./api/routes/users');
 
 var app = express();
 
